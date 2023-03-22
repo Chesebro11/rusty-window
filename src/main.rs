03f5964f9b59b743eb93fa16dee8bdf9dfcadb26
Jared Chesebro <jaredchesebro@novuslabs.com>
2023-03-22 11:59:13 -0700

    window logic moved to lib, added esc key close, start wgpu

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,5 @@
-use winit::{event_loop::EventLoop, window::WindowBuilder, dpi::PhysicalSize};
+use Rusty_Window::run;
 
 fn main () {
-    let event_loop = EventLoop::new();
-    let window_builder = WindowBuilder::new()
-        .with_title("My Window")
-        .with_inner_size(PhysicalSize::new(800, 600));
-
-    let window = window_builder.build(&event_loop).unwrap();
-
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = winit::event_loop::ControlFlow::Wait;
-        match event {
-            winit::event::Event::WindowEvent { event, .. } => match event {
-                winit::event::WindowEvent::CloseRequested => {
-                    *control_flow = winit::event_loop::ControlFlow::Exit
-                },
-                _ => (),
-            },
-            _ => (),
-        }
-    })
+    run();
 }
